6c131e4429b6b652e9e70aa4425e40a583874c79
drewrobertsmith <drew.robert.smith@gmail.com>
2024-03-08 15:49:12 -0600

    Setup Trackplayer and playback service

--- src/app/(tabs)/index.js
+++ src/app/(tabs)/index.js
@@ -1,5 +1,34 @@
-import { Text } from "react-native";
+import { ActivityIndicator, Text } from "react-native";
+import {
+  addTracks,
+  playbackService,
+  setupPlayer,
+} from "../../../trackPlayerServices";
+import { useEffect, useState } from "react";
+
+import TrackPlayer from "react-native-track-player";
+
+TrackPlayer.registerPlaybackService(() => playbackService);
 
 export default function App() {
-  return <Text>Stations</Text>;
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
+  //this intantiates the player using the setupPlayer() function from the trackservice
+  useEffect(() => {
+    async function setup() {
+      const isSetup = await setupPlayer(); // The player is ready to be used
+      const queue = await TrackPlayer.getQueue();
+      if (isSetup && queue.length <= 0) {
+        await addTracks();
+      }
+      setIsPlayerReady(isSetup);
+      console.log("player is ready");
+    }
+    setup();
+  }, []);
+
+  if (!isPlayerReady) {
+    return <ActivityIndicator size="large" />;
+  } else {
+    return <Text>Stations</Text>;
+  }
 }
