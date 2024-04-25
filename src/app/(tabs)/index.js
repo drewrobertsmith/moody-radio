6ebacf4c87e0918bd0976086fb4840cfed576497
drewrobertsmith <drew.robert.smith@gmail.com>
2024-04-25 10:15:59 -0500

    added sentry.io for logging

--- src/app/(tabs)/index.js
+++ src/app/(tabs)/index.js
@@ -10,7 +10,7 @@ import TrackPlayer from "react-native-track-player";
 
 TrackPlayer.registerPlaybackService(() => playbackService);
 
-export default function App() {
+const App = () => {
   const [isPlayerReady, setIsPlayerReady] = useState(false);
   //this intantiates the player using the setupPlayer() function from the trackservice
   useEffect(() => {
@@ -29,6 +29,12 @@ export default function App() {
   if (!isPlayerReady) {
     return <ActivityIndicator size="large" />;
   } else {
-    return <Text>Hello World</Text>;
+    return (
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    );
   }
-}
+};
+
+export default App;
--- src/app/_layout.js
+++ src/app/_layout.js
@@ -1,12 +1,38 @@
+import * as Sentry from "@sentry/react-native";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Stack, useNavigationContainerRef } from "expo-router";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
-import { Stack } from "expo-router";
 import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
+import { useEffect } from "react";
 
-export default function AppLayout() {
+// Construct a new instrumentation instance. This is needed to communicate between the integration and React
+const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
+
+Sentry.init({
+  dsn: "https://dc0fec916165c76a4bb2c0234356c357@o4507147687821312.ingest.us.sentry.io/4507147712266240",
+  debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  integrations: [
+    new Sentry.ReactNativeTracing({
+      // Pass instrumentation to be used as `routingInstrumentation`
+      routingInstrumentation,
+      // ...
+    }),
+  ],
+});
+
+function AppLayout() {
+  // Capture the NavigationContainer ref and register it with the instrumentation.
+  const ref = useNavigationContainerRef();
+  useEffect(() => {
+    if (ref) {
+      routingInstrumentation.registerNavigationContainer(ref);
+    }
+  }, [ref]);
+
   const queryClient = new QueryClient({
     // defaultOptions: {
     //   cacheTime: 1000 * 60 * 60 * 24, // 24 hours
@@ -35,3 +61,5 @@ export default function AppLayout() {
     </GestureHandlerRootView>
   );
 }
+
+export default Sentry.wrap(AppLayout);
